1c18ce9fb60ff9d0b1b0cf09958a4afbfd5e31a3
luoxy <3366018479@qq.com>
2024-07-15 14:38:55 +0800

    整合及提取工具类;

--- src/main/java/utils/ArraysUtil.java
+++ src/main/java/utils/ArraysUtil.java
@@ -43,17 +43,6 @@ public class ArraysUtil {
         System.out.println();
     }
 
-    public static void insertionSort(int[] arr) {
-        if (arr == null || arr.length < 2) {
-            return;
-        }
-        for (int i = 1; i < arr.length; i++) { // 0 ~ i 做到有序
-            for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
-                swap(arr, j, j + 1);
-            }
-        }
-    }
-
     public static int[] generateRandomArray(int maxSize, int maxValue) {
         // Math.random() [0,1)
         // Math.random() * N [0,N)
